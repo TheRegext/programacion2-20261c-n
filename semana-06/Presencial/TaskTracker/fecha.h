6c0eb1fdaca03c39db8613c43b077e90f9c97f0c
Angel <43154124+angel-utn@users.noreply.github.com>
2026-04-23 20:47:05 -0300

    Archivos de la clase de Semana 06

---
+++ semana-06/Presencial/TaskTracker/fecha.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+class Fecha{
+public:
+  Fecha();
+  Fecha(int dia, int mes, int anio);
+
+  /// getters y setters
+  void setDia(int dia);
+  void setMes(int mes);
+  void setAnio(int anio);
+
+  int getDia() const;
+  int getMes() const;
+  int getAnio() const;
+
+  std::string toString();
+
+private:
+    int _dia, _mes, _anio;
+};
---
+++ semana-06/Presencial/TaskTracker/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include "TareaGestor.h"
+
+int main()
+{
+    TareaGestor gestor;
+    //gestor.Crear();
+    //gestor.Modificar();
+    cout << endl;
+
+    gestor.ListarTodas();
+
+    return 0;
+}
